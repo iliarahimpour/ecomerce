5a5e86040221e7e5f4585fdbe78181f125ebd66c
unknown <iliaamm148@gmail.com>
2023-01-12 21:01:23 +0330

    Started to main projects

--- ecommerce/market/views.py
+++ ecommerce/market/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
-
-
+from .models import Shirt , Shoes ,Jeans , Category
+from django.utils import timezone
+from django.http import HttpResponse , Http404
 # Create your views here.
 
 
-
 def home(request):
-     return render(request, "market/homepage.html", {})
+     contex=Shoes.objects.all()[0:3]
+     return render(request, "market/homepage.html", {"shoes":contex})
+
+
+def product(request , pk):
+     try :
+          pk=Shoes.objects.get(pk=pk)
+          return render(request ,"market/product_dedatil.html" ,{"product":pk} )
+     except:
+          return Http404("The page ,wich you were looking , cannot find!")
+
+
+def all(request):
+     try :
+          context=Shoes.objects.all()
+          return render(request ,"market/product_dedatil.html" ,{"shoes":context} )
+     except:
+          return Http404("The page ,wich you were looking , cannot find!")
