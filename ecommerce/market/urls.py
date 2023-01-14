7b9526215276009f1a26c0e687b811b6bc7b68cf
unknown <iliaamm148@gmail.com>
2023-01-14 11:57:53 +0330

    Added products detail car

--- ecommerce/market/urls.py
+++ ecommerce/market/urls.py
@@ -3,9 +3,9 @@ from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
-    path('' , views.home , name="home"),
-    path('product/<int:pk>/' , views.product , name='product'),
-    path('product/shoes/' , views.all , name='all'),
+    path('', views.home, name="home"),
+    path('product_detail/<int:pk>/', views.product_detail, name='product_detail'),
+    path('product/shoes/', views.all, name='all'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
